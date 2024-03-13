2afa2aba7a8cf1b83f59e3d831021c275b499df6
Alberto Gómez Serrano <smrgomezal@gmail.com>
2024-03-13 20:24:35 +0100

    Refactorizacion final

--- src/main/kotlin/fecha.kt
+++ src/main/kotlin/fecha.kt
@@ -3,10 +3,10 @@
  *
  * @property dia
  * @property mes
- * @property a
+ * @property anio
  * @constructor Create empty Fecha
  */
-class Fecha(private val dia: Int, private val mes: Int, private val a: Int) {
+class Fecha(private val dia: Int, private val mes: Int, private val anio: Int) {
 
     /**
      * Valida
@@ -14,26 +14,33 @@ class Fecha(private val dia: Int, private val mes: Int, private val a: Int) {
      * @return true o false
      */
     fun valida(): Boolean {
+        var esValida = false
 
-        if (dia < 1 || dia > 31) return false
-        if (mes < 1 || mes > 12) return false
+        if ((dia >= 1 || dia <= 31) && (mes >=1  && mes <= 12)) {
+            var diasMes = diasdelMes(mes)
+            esValida = dia <= diasMes
+        }
 
         // determinamos la cantidad de días del mes:
-        return diasdelMes()
+        return esValida
     } // … más métodos
+
+    private fun esBisiesto(a: Int) : Boolean {
+        return (a % 400 == 0 || a % 4 == 0  && a % 100 != 0)
+    }
 
     /**
      * Dias del mes
      * Comprueba di el dia introducido es valido
      * @return true o false
      */
-    fun diasdelMes () : Boolean {
+    private fun diasdelMes (mes : Int) : Int {
         var diasMes = 0
         when (mes) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
             4, 6, 9, 11 -> diasMes = 30
-            2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
+            2 -> if(esBisiesto(anio)) 29 else 28
         }
-        return if (dia > diasMes) false else true
+        return diasMes
     }
 }
